eca209835f6cee583d006d75d40254223488b42d
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-03 14:38:50 +0800

    传送门的特效大概制作

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Circle : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 speed = new Vector3(0,0,90f);
+
+    private void Update ()
+    {
+        transform.Rotate(speed * Time.deltaTime);
+	}
+}
